8749f12d4522884f4d6cce04730a5c4a83156a12
SecretAccount981 <198354452+SecretAccount981@users.noreply.github.com>
2025-11-07 15:17:51 +0100

    1.0.4

--- src/assets/js/panels/home.js
+++ src/assets/js/panels/home.js
@@ -125,7 +125,7 @@ class Home {
                 path: `${dataDirectory}/${process.platform == 'darwin' ? this.config.dataDirectory : `.${this.config.dataDirectory}`}`,
                 version: this.config.game_version,
                 detached: launcherSettings.launcher.close === 'close-all' ? false : true,
-                downloadFileMultiple: 30,
+                downloadFileMultiple: 10, // était a 30
                 loader: {
                     type: this.config.loader.type,
                     build: this.config.loader.build,
